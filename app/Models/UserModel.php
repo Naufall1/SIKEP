ea20b1ba000f557552406130f8890888e7f2b69a
Naufal <naufalkurniawan605@gmail.com>
2024-03-28 17:09:39 +0700

    modify model name for `UserModel`

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
     protected $table = 'user';
