61938fec1d2e47bade5b91275b4b2a11e4e705a9
Amanda Vanika <amandavanika4@gmail.com>
2024-03-30 21:03:30 +0700

    add seeder (demografi & have_demografi)

---
+++ database/seeders/DemografiSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DemografiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('demografi')->insert([
+            [
+                'demografi_id' => 1,
+                'user_id' => '2',
+                'jenis' => 'Migrasi Keluar'
+            ],
+            [
+                'demografi_id'=>2,
+                'user_id' => '2',
+                'jenis' => 'Migrasi Keluar'
+            ],
+            [
+                'demografi_id' => 3,
+                'user_id' => '2',
+                'jenis' => 'Kematian'
+            ],
+        ]);
+    }
+}
