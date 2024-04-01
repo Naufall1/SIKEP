94849b776cc395841eace323e4793a901f6904e5
Naufal <naufalkurniawan605@gmail.com>
2024-04-01 09:01:06 +0700

    add `warga` controller

---
+++ app/Http/Controllers/WargaController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WargaModel;
+use App\Models\WargaModifiedModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class WargaController extends Controller
+{
+    public function index(){
+        $warga = WargaModel::all();
+        return view('penduduk.warga.index', compact('warga'));
+    }
+    public function create(){
+        return view('penduduk.warga.tambah');
+    }
+    public function store(Request $request){
+        WargaModel::create($request->only([
+            'NIK',
+            'no_kk',
+            'nama',
+            'tempat_lahir',
+            'tanggal_lahir',
+            'jenis_kelamin',
+            'agama',
+            'status_perkawinan',
+            'status_keluarga',
+            'status_warga',
+            'jenis_pekerjaan',
+            'penghasilan',
+            'kewarganegaraan',
+            'pendidikan',
+            'no_paspor',
+            'no_kitas',
+            'nama_ayah',
+            'nama_ibu',
+        ]));
+        return redirect()->route('warga');
+    }
+    public function edit($nik){
+        $warga = WargaModel::find($nik);
+        return view('penduduk.warga.edit', compact('warga'));
+    }
+    public function update(Request $request, $nik){
+        WargaModifiedModel::create([
+            'NIK' => $request->nik,
+            'user_id' => Auth::user()->user_id,
+            'agama' => $request->agama,
+            'status_perkawinan'=> $request->status_perkaeinan,
+            'status_keluarga'=> $request->status_keluarga,
+            'status_warga'=> $request->status_warga,
+            'jenis_pekerjaan'=> $request->jenis_pekerjaan,
+            'penghasilan'=> $request->penghasilan,
+            'pendidikan'=> $request->pendidikan,
+            'tanggal_request' => now(),
+            'status_request' => 'menunggu',
+        ]);
+        return redirect()->route('warga');
+    }
+}
