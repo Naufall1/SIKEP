ff10ed9eccb477a8325d3f0aeda7f218c6b68402
Naufal <naufalkurniawan605@gmail.com>
2024-04-09 14:00:32 +0700

    update `WargaModified` migration (add no_kk)

--- database/migrations/2024_03_23_082921_create_warga_modified.php
+++ database/migrations/2024_03_23_082921_create_warga_modified.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('wargaModified', function (Blueprint $table) {
             $table->id('id_modify_warga');
             $table->char('NIK', 16);
+            $table->char('no_kk', 16);
             $table->unsignedBigInteger('user_id');
             $table->enum('agama', ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']);
             $table->enum('status_perkawinan', ['Kawin', 'Belum Kawin', 'Cerai Hidup', 'Cerai Mati']);
