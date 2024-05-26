dc080c2d1715f7713b3b42cbdea2a99112f94ec8
Naufal <naufalkurniawan605@gmail.com>
2024-05-26 17:47:21 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -193,7 +193,7 @@ Route::prefix('article_announcements')->middleware('auth')->group(function () {
 Route::prefix('admin')->middleware('role:rw')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin');
     Route::post('/list', [AdminController::class, 'list'])->name('admin.list');
-    Route::get('/{username}', [AdminController::class, 'show'])->name('admin.detail');
     Route::get('/tambah', [AdminController::class, 'create'])->name('admin.create');
+    Route::get('/{username}', [AdminController::class, 'show'])->name('admin.detail');
     Route::post('/tambah', [AdminController::class, 'store'])->name('admin.store');
 });
