01a10e9ea3279723c1218ac774e402d73d73e54c
Naufal <naufalkurniawan605@gmail.com>
2024-05-26 15:28:24 +0700

    implement datatables daftar bansos

--- resources/views/bansos/perhitungan/detail.blade.php
+++ resources/views/bansos/perhitungan/detail.blade.php
@@ -113,7 +113,12 @@
                             <h2 class="">Daftar Bansos</h2>
                             <div class="tw-flex tw-flex-col tw-gap-3">
 
+
                                 <table class="tw-w-[702px] md:tw-w-full" id="daftarBansos">
+                                    <button id="buttonAddBansos"
+                                                    class="tw-h-10 tw-w-fit tw-px-4 tw-bg-b500 tw-text-n100 tw-font-sans tw-font-bold tw-text-[14px] tw-rounded-md hover:tw-bg-b600 active:tw-bg-b700 tw-flex tw-items-center">
+                                                    Tambah
+                                                </button>
                                     <thead class="">
                                         <tr class="">
                                             <th>No</th>
@@ -200,6 +205,63 @@
                 $('html, body').css({
                     overflow: 'auto',
                 });
+            });
+            dataBansos = $('#daftarBansos').DataTable({
+                serverSide: true, // serverSide: true, jika ingin menggunakan server side processing
+                ajax: {
+                    "url": "{{ route('penduduk.keluarga.detail.listBansos', ['no_kk' => $dataKeluarga->no_kk]) }}",
+                    "dataType": "json",
+                    "type": "POST",
+                },
+                createdRow: function(row, data, dataIndex) {
+                    $(row).addClass("tw-h-16 hover:tw-bg-n300 tw-flex");
+                },
+                drawCallback: function() {
+                    $('.table.dataTable').css('border-collapse', 'collapse');
+                    $('.dataTables_empty').html(`<svg xmlns="http://www.w3.org/2000/svg" width="120" height="121" fill="none" viewBox="0 0 150 151">
+                        <g clip-path="url(#a)">
+                            <path fill="#E3E3E3" d="M75 150.5c41.421 0 75-33.579 75-75S116.421.5 75 .5 0 34.079 0 75.5s33.579 75 75 75Z"/>
+                            <path fill="#fff" d="M120 150.5H30v-97a16.018 16.018 0 0 0 16-16h58a15.906 15.906 0 0 0 4.691 11.308A15.89 15.89 0 0 0 120 53.5v97Z"/>
+                            <path fill="#0284FF" d="M75 102.5c13.255 0 24-10.745 24-24s-10.745-24-24-24-24 10.745-24 24 10.745 24 24 24Z"/>
+                            <path fill="#fff" d="M83.485 89.814 75 81.329l-8.485 8.485-2.829-2.829 8.486-8.485-8.486-8.485 2.829-2.829L75 75.672l8.485-8.486 2.829 2.829-8.486 8.485 8.486 8.485-2.829 2.829Z"/>
+                            <path fill="#CCE4FF" d="M88 108.5H62a3 3 0 1 0 0 6h26a3 3 0 1 0 0-6Zm9 12H53a3 3 0 1 0 0 6h44a3 3 0 1 0 0-6Z"/>
+                        </g>
+                        <defs>
+                            <clipPath id="a">
+                            <rect width="150" height="150" y=".5" fill="#fff" rx="75"/>
+                            </clipPath>
+                        </defs>
+                        </svg>
+                        <p class="tw-placeholder tw-font-semibold">Tidak ada data</p>
+                    `);
+                },
+                paging: false,
+                info: false,
+                searching: false,
+                order: [
+                    [1, 'asc']
+                ],
+                columns: [{
+                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
+                    className: "tw-w-[44px]",
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "bansos_nama",
+                    className: "tw-grow",
+                    orderable: true,
+                    searchable: false
+                }, {
+                    data: "tanggal_menerima",
+                    className: "tw-w-[196px]",
+                    orderable: true,
+                    searchable: false
+                }, {
+                    data: "keterangan",
+                    className: "tw-w-[140px]",
+                    orderable: false,
+                    searchable: false
+                }]
             });
         });
     </script>
