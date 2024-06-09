5bcaa4dc052dd2d3e132b5e18c21651087774298
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-06-09 08:47:12 +0700

    slicing checkbox form tambah publikasi and delete detail publikasi view (should be on draf)

--- resources/views/publikasi/tambah.blade.php
+++ resources/views/publikasi/tambah.blade.php
@@ -1,7 +1,8 @@
 @extends('layout.layout', ['isForm' => true])
 
 @section('content')
-    <div class="tw-pt-[100px] tw-mx-5 md:tw-mx-auto md:tw-w-[702px] tw-flex tw-flex-col tw-gap-2 tw-pb-10 tw-animate-fade-right tw-animate-ease-in-out tw-animate-duration-[500ms]">
+    <div
+        class="tw-pt-[100px] tw-mx-5 md:tw-mx-auto md:tw-w-[702px] tw-flex tw-flex-col tw-gap-2 tw-pb-10 tw-animate-fade-right tw-animate-ease-in-out tw-animate-duration-[500ms]">
         <p class="tw-breadcrumb tw-text-n500">Daftar Publikasi /
             <span class="tw-font-bold tw-text-b500">Tambah Publikasi</span>
         </p>
@@ -21,16 +22,16 @@
                         <div class="tw-flex tw-flex-col tw-gap-3">
 
                             <x-input.label for="judul" label="Judul">
-                                <x-input.input type="text" name="judul"
-                                    placeholder="Masukkan Judul" value="{{old('judul')}}"></x-input.input>
+                                <x-input.input type="text" name="judul" placeholder="Masukkan Judul"
+                                    value="{{ old('judul') }}"></x-input.input>
                                 @error('judul')
                                     <small class="form-text tw-text-red-600">{{ $message }}</small>
                                 @enderror
                             </x-input.label>
 
                             <x-input.label for="penulis" label="Nama Penulis">
-                                <x-input.input type="text" name="penulis"
-                                    placeholder="{{$user->nama}}" disabled></x-input.input>
+                                <x-input.input type="text" name="penulis" placeholder="{{ $user->nama }}"
+                                    disabled></x-input.input>
                                 @error('penulis')
                                     <small class="form-text tw-text-red-600">{{ $message }}</small>
                                 @enderror
@@ -44,7 +45,7 @@
                             </x-input.label>
 
                             <x-input.label for="caption" label="Caption">
-                                <x-input.input type="text" name="caption" value="{{old('caption')}}"
+                                <x-input.input type="text" name="caption" value="{{ old('caption') }}"
                                     placeholder="Masukkan Caption"></x-input.input>
 
                                 @error('caption')
@@ -53,7 +54,7 @@
                             </x-input.label>
 
                             <x-input.label required for="content" label="Isi">
-                                <x-input.textarea name="isi" placeholder="Masukkan Isi Publikasi"
+                                <x-input.textarea class="tw-h-56" name="isi" placeholder="Masukkan Isi Publikasi"
                                     value="{{ old('isi', isset($formState['isi']) ? $formState['isi'] : '') }}">
                                 </x-input.textarea>
                                 @error('isi')
@@ -61,19 +62,26 @@
                                 @enderror
                             </x-input.label>
 
-                            <x-input.label class="tw-relative" for="kategori-list" label="Kategori">
-                                <x-input.select2 name="kategori"
-                                    placeholder="Pilih Kategori" selected="{{old('kategori')}}"></x-input.select2>
-
-                                @error('kategori')
+                            {{-- <x-input.label class="tw-relative" for="kategori" label=""> --}}
+                                {{-- <x-input.select2 name="kategori" placeholder="Pilih Kategori"
+                                    selected="{{ old('kategori') }}"></x-input.select2> --}}
+                                    <x-input.input type="hidden" name="kategori" value="Pengumuman"
+                                    placeholder=""></x-input.input>
+                                {{-- @error('kategori')
                                     <small class="form-text tw-text-red-600">{{ $message }}</small>
-                                @enderror
-                            </x-input.label>
-                            <br>
-                            <x-input.label for="status" label="Apakah anda akan langsung posting publikasi ini?">
-                                <div class="tw-flex items-center">
-                                    <input type="checkbox" name="status" id="status" value="Ditampilkan" class="tw-form-checkbox">
-                                    <label for="status" class="tw-ml-2">Ya, Langsung Posting</label>
+                                @enderror --}}
+                            {{-- </x-input.label> --}}
+                            
+                            <x-input.label for="status" label="">
+
+                                <input type="checkbox" name="status" id="status" value="Ditampilkan" class="tw-peer tw-hidden">
+                                <div id="statusContainer"
+                                    class="tw-flex tw-gap-2 items-center tw-w-full tw-p-3 tw-bg-n100 tw-group tw-items-center peer-checked:tw-bg-b50 tw-border-[1.5px] tw-rounded-md tw-cursor-pointer tw-border-n400 peer-checked:tw-border-b200">
+                                    <div id="iconTick" class="tw-w-5 tw-h-5 tw-bg-[url('/public/img/uncheck.svg')]"></div>
+                                    <div class="tw-flex tw-flex-col tw-gap-1">
+                                        <h4 class="tw-font-semibold tw-font-sans tw-text-base tw-text-n1000">Langsung Unggah Pengumuman?</h4>
+                                        <p class="tw-font-medium tw-font-sans tw-text-sm tw-text-n800">Setelah tersimpan, pengumuman akan langsung ditampilkan di Halaman Utama</p>
+                                    </div>
                                 </div>
 
                                 @error('status')
@@ -104,15 +112,27 @@
 @endsection
 
 @push('js')
-<script>
+    <script>
+        function updatePublishDate() {
+            var currentDate = new Date().toISOString().slice(0, 10);
+            document.getElementById('tanggal_publish').value = currentDate;
+        }
 
-    function updatePublishDate() {
-        var currentDate = new Date().toISOString().slice(0, 10);
-        document.getElementById('tanggal_publish').value = currentDate;
-    }
+        function clearPublishDate() {
+            document.getElementById('tanggal_publish').value = '';
+        }
 
-    function clearPublishDate() {
-        document.getElementById('tanggal_publish').value = '';
-    }
-</script>
+        $(document).ready(function () {
+            $(document).on('change', '#status', function () {
+                console.log($(this).is(':checked'));
+                if ($(this).is(':checked')) {
+                    $('#iconTick').removeClass("tw-bg-[url('/public/img/uncheck.svg')]");
+                    $('#iconTick').addClass("tw-bg-[url('/public/img/checked.svg')]");
+                } else {
+                    $('#iconTick').removeClass("tw-bg-[url('/public/img/checked.svg')]");
+                    $('#iconTick').addClass("tw-bg-[url('/public/img/uncheck.svg')]");
+                }
+            });
+        });
+    </script>
 @endpush
